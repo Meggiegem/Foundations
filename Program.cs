621fbefb2016956d660d4a833c1295441d6b7cd1
Meggiegem <Meggiegem@users.noreply.github.com>
2016-07-24 12:58:49 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_1
+namespace Homework4
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int fact;
+            int x = 1;
+            Console.WriteLine("Please enter your number to be factored:");
+            fact = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= fact; i++)
+            {
+                x = x * i ;
+
+            }
+            Console.WriteLine("your Factorial =");
+            Console.WriteLine(x);
+            Console.ReadKey();
         }
+
     }
 }
